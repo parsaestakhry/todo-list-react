3a439fd3619a6056cf2f86ac310eba27bf7d6d82
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-10 12:09:42 +0330

    commit

--- todo-react/src/components/ToDoList.jsx
+++ todo-react/src/components/ToDoList.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
 export const ToDoList = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    "Eat Breakfast",
+    "take a shower",
+    "Be god Of War",
+  ]);
   const [newTask, setNewTask] = useState("");
 
-  const handleInputChange = (event) => {};
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
-  const addTask = () => {};
+  const addTask = () => {
+    setTasks([...tasks, newTask])
+  };
 
-  const removeTask = (index) => {};
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
 
   const moveTaskUp = (index) => {};
 
@@ -16,7 +26,39 @@ export const ToDoList = () => {
 
   return (
     <>
-      <div></div>
+      <div className="to-do-list">
+        <h1 className="">To-Do-List</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter a task ..."
+            value={newTask}
+            onChange={handleInputChange}
+          />
+          <button className="add-button" onClick={addTask}>
+            Add
+          </button>
+        </div>
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button
+                className="delete-button"
+                onClick={() => removeTask(index)}
+              >
+                Delete
+              </button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>
+                UP
+              </button>
+              <button className="move-button" onClick={() => moveTaskDown(index)}>
+                DOWN
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
   );
 };
